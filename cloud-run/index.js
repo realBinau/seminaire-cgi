ad69344ffcbddbfdb7fb2438f221d9108a92ab27
Binau <realbinau@gmail.com>
2025-03-15 16:27:43 +0100

    Initial commit

--- cloud-run/index.js
+++ cloud-run/index.js
@@ -1,13 +1,13 @@
 import http from 'http'
 
 const port = process.env.PORT || 3000
-console.log(`-- Do setup --`)
+console.log(`-- Do start --`)
 
 http
     // MANAGE REQUEST
     .createServer((req, res) => {
-        res.writeHead(200, {'Content-Type': 'application/json'})
         console.log(`-- Request --`)
+        res.writeHead(200, {'Content-Type': 'application/json'})
         res.end(`Hello Cgi from Cloud Run`)
     })
     // LISTEN
